712f842e2b7cf58bab943311d4bccd8c99adab4b
KungFuThai <101091168+KungFuThai@users.noreply.github.com>
2023-12-18 18:17:32 +0700

    update forget password

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -7,18 +7,23 @@ use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Traits\ImageStorageTrait;
 use App\Http\Traits\ResponseTrait;
+use App\Models\ForgetPassword;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
     use ResponseTrait;
     use ImageStorageTrait;
-    public function getAuth(){
+
+    public function getAuth()
+    {
         return $this->successResponse(auth()->user());
     }
+
     public function login(LoginRequest $request)
     {
         $credentials = [
@@ -39,7 +44,7 @@ class AuthController extends Controller
     public function register(RegisterRequest $request)
     {
         $path = null;
-        if($request->has('avatar')){
+        if ($request->has('avatar')) {
             $path = $this->storeImage('avatars', $request->file('avatar'));
         }
         $userAttributes = $request->validated();
@@ -58,5 +63,50 @@ class AuthController extends Controller
         $request->user()->currentAccessToken()->delete();
 
         return $this->successResponse(message: 'Đăng xuất thành công!');
+    }
+
+    public function fogetPassword(Request $request)
+    {
+        $email = $request->email;
+        $check = User::query()->where('email', $email)->exists();
+        if ($check){
+            $forgetPassword = ForgetPassword::query()
+                ->updateOrCreate(
+                    ['email' => $email],
+                    [
+                        'email' => $email,
+                        'token' => random_int(100000, 999999),
+                    ]
+                );
+
+            Mail::send('mail', compact('forgetPassword'), function ($email) use ($forgetPassword) {
+                $title = config('app.name') . ' ' . 'đổi mật khẩu';
+                $email->subject(config('app.name') . ' ' . 'đổi mật khẩu');
+                $email->to($forgetPassword->email);
+            });
+
+            return $this->successResponse($forgetPassword['token'],'Gửi mail thành công hãy kiểm tra email của bạn!');
+        }
+        return $this->errorResponse('Email không tồn tại!');
+    }
+
+    public function resetPassword(Request $request)
+    {
+        $token = $request->token;
+        $email = ForgetPassword::query()
+            ->where('token', $token)
+            ->pluck('email')
+            ->toArray();
+        $password = $request->password;
+
+        User::query()
+            ->where('email', $email)
+            ->update([
+                'password' => Hash::make($password),
+            ]);
+
+        ForgetPassword::query()->where('token', $token)->delete();
+
+        return $this->successResponse(message: 'Đặt lại mật khẩu thành công! Hãy đăng nhập lại!');
     }
 }
